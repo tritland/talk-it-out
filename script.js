90e79ff4b51e85f98a3bdfd797ea03deadaad06f
Antoinette <smith@antoinetteauthedna.com>
2017-05-10 16:28:37 -0500

    fixed a few typos

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 /*
-1. Start with the number 42 and set that equal to `majorKey`
+1. Start with the number 42 and set that equal to the variable `majorKey`
 
-2. Create condition logic to check if the `majorKey` is great or equal to 53
+2. Create conditional logic to check if `majorKey` is greater than or equal to 53
 2-1. If true, add 42 to `majorKey`
 2-2. If false, subtract 13 from `majorKey`
 
-3. Add a the string witha  value of 11 to `majorKey`
+3. Add a string with a value of 11 to `majorKey`
 
-4. Create an array, loop through `majorKey` using `charAt`, set array[i] to each value
+4. Create an array named `keyChars`, then loop through `majorKey` using the `charAt` method and set `keyChars[i]` to each value
 
-5. Remove the first and last values from the array
+5. Remove the first and last values from `keyChars`
 
-6. Create a new variable. Loop **backwards** through the array and store each value into the new variable, combining each of the values of that array
+6. Create a new variable. Loop **backwards** through `keyChars` and store each value into the new variable, combining each of the values of `keyChars`
 
 7. `parseInt` both the `majorKey` and the new variable created in Step 6, ensure that both `majorKey` and the new variable are set to these new parsed values.
 
@@ -25,11 +25,13 @@ If neither of these are true, set `majorKey` to 2.
 10. Create a while loop that counts down from 10 and increments `majorKey` by 1.
 
 11. Create a function that accepts an argument `val`. The function should convert `val` to a string, then drop the first character off the string, but only if there
-is more than one character in the string. Return `val` and set the `majorKey` to the returned value.
+is more than one character in the string. Return `val` and set `majorKey` to the returned value.
 
 12. Call the function.
 
-13. Console log `majorKey`. Create an index.html file, link in this Javascript file. Load in the browser, check the value.
+13. Console log `majorKey`. 
 
-14. Your answer should be a string value that equals 7. Is that what you got?
+14. Create an index.html file, link in this JavaScript file. Load in the browser and check the value from Step 13.
+
+15. Your answer should be a string value that equals 7. Is that what you got?
 */
